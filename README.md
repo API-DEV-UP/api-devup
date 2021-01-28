fed8f88dce74f1beb0b2fe8d2e7edc742b5227b4
ZeU4S <73115312+zeu45@users.noreply.github.com>
2021-01-28 23:46:00 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+<p align="center">
+<a href="https://www.npmjs.com/package/api-devup"><img src="https://img.shields.io/npm/v/api-devup.svg?style=flat-square" alt="NPM version"></a>
+<a href="https://www.npmjs.com/package/api-devup"><img src="https://img.shields.io/npm/dt/api-devup.svg?style=flat-square" alt="NPM downloads"></a>
+</p>
+
+API-DEVUP is a powerful [Node.js](https://nodejs.org) librarys is intended for interaction with the service [DEV-UP.RU](https://dev-up.ru) 🚀
+
+| 📖 [Documentation](https://www.npmjs.com/package/api-devup) | 🤖 [Author](https://vk.com/zeuvs) |
+| ----------------------------------------------------------- | --------------------------------- |
+
+## Features
+
+- Works with large collections of data
+- Easy authorization form
+
+## Installation
+
+> **[Node.js](https://nodejs.org/) 12.0.0 or newer is required**
+
+### NPM
+
+```
+npm i api-devup --save
+```
+
+### Usage example
+
+```js
+const API = require("api-devup");
+
+const api = new API({
+  key: process.env.KEY,
+});
+
+async function run() {
+  const response = await api.getGroups({
+    user_id: 1,
+  });
+  console.log(response);
+}
+
+run().catch(console.log);
+```
---
+++ index.js
@@ -0,0 +1,161 @@
+const fetch = require("node-fetch");
+class Auth {
+  constructor(key) {
+    this.settings = {
+      auth: key,
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      url: {
+        getGroups: "https://api.dev-up.ru/method/vk.getGroups",
+        getApps: "https://api.dev-up.ru/method/vk.getApps",
+        getStickers: "https://api.dev-up.ru/method/vk.getStickers",
+        shortLink: "https://api.dev-up.ru/method/utils.getShortLink",
+        speech: "https://api.dev-up.ru/method/audio.speech",
+        profile: "https://api.dev-up.ru/method/profile.get",
+      },
+    };
+  }
+  async call(method, params) {
+    const res = await fetch(`https://api.dev-up.ru/method/${method}`, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+        ...params,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+  async shortLink(params) {
+    if (params && !params.url) {
+      return {
+        err: `The "url" parameter was not passed in the request. This parameter can be passed as follows: { url: "link to audio" }, set the correct order of parameters and try again!`,
+      };
+    }
+    const res = await fetch(this.settings.url.shortLink, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+        ...params,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+  async audioSpeech(params) {
+    if (params && !params.url) {
+      return {
+        err: `The "url" parameter was not passed in the request. This parameter can be passed as follows: { url: "link to audio" }, set the correct order of parameters and try again!`,
+      };
+    }
+    const res = await fetch(this.settings.url.speech, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+        ...params,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+  async getProfile(params) {
+    if (params) {
+      return { err: "This method does not need parameters!" };
+    }
+    const res = await fetch(this.settings.url.profile, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+  async getApps(params) {
+    if (params && !params.user_id) {
+      return {
+        err: 'The key parameter was not passed. Example {"user_id": number }!',
+      };
+    }
+    const res = await fetch(this.settings.url.getApps, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+        ...params,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+  async getGroups(params) {
+    if (!params.user_id) {
+      return {
+        err: `The key parameter was not passed. Example {"user_id": number }!`,
+      };
+    }
+    const res = await fetch(this.settings.url.getGroups, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+        ...params,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+  async getStickers(params) {
+    if (!params.user_id) {
+      return {
+        err: 'The key parameter was not passed. Example {"user_id": number }!',
+      };
+    }
+    const res = await fetch(this.settings.url.getStickers, {
+      method: this.settings.method,
+      credentials: this.settings.credentials,
+      body: JSON.stringify({
+        key: this.settings.auth.key,
+        ...params,
+      }),
+      headers: this.settings.headers,
+    });
+    this.res = await res.json();
+    if (this.res.err) {
+      return this.res;
+    }
+    return this.res.response;
+  }
+}
+module.exports = Auth;
